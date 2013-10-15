852e48e307dffac0eb2bf799d04e80c4b71e2345
jas- <jason.gerfen@gmail.com>
2013-10-15 13:57:16 -0600

    Tue Oct 15 13:57:16 MDT 2013 - Updated response from post request (with logging data)

--- routes/spkac.js
+++ routes/spkac.js
@@ -2,7 +2,19 @@
 /*
  * POST spkac.
  */
+var crypto = require('crypto');
 
 exports.index = function(req, res){
+  var spki = new crypto.Certificate();
+  var spkac = new Buffer(stripLineEndings(req.body.spkac), 'utf-8');
+  
+  console.log(spki.verifySpkac(spkac));
+  console.log(spki.exportChallenge(spkac).toString('utf-8'));
+  console.log(spki.exportPublicKey(spkac).toString('utf-8'));
+
   res.send("respond with a resource");
 };
+
+function stripLineEndings(obj) {
+  return obj.replace(/(\r\n|\n|\r)/gm, '');
+}
