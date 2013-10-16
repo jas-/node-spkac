7fd0b9ad665ca21b26bf57d63a06d39a10b23adf
Jas- <jason.gerfen@gmail.com>
2013-10-16 07:59:40 -0600

    Wed Oct 16 07:59:40 MDT 2013 - Updated example to include validation of required fields, hodge podge of UI prettifications & detailed comments regarding steps taken to verify, export challenge & export public key from SPKAC sent from <keygen> element

--- routes/spkac.js
+++ routes/spkac.js
@@ -1,18 +1,63 @@
 
-/*
- * POST spkac.
- */
-var crypto = require('crypto');
+var crypto = require('crypto'),
+  spki = new crypto.Certificate();
 
 exports.index = function(req, res){
-  var spki = new crypto.Certificate();
-  var spkac = new Buffer(stripLineEndings(req.body.spkac), 'utf-8');
-  
-  console.log(spki.verifySpkac(spkac));
-  console.log(spki.exportChallenge(spkac).toString('utf-8'));
-  console.log(spki.exportPublicKey(spkac).toString('utf-8'));
 
-  res.send("respond with a resource");
+  /* Array of required fields */
+  var required = [
+    'commonName',
+    'emailAddress',
+    'countryName',
+    'stateOrProvinceName',
+    'localityName',
+    'organizationName',
+    'organizationalUnitName',
+    'spkac'
+  ];
+  
+  var errors = [];
+
+  /* Ensure we have everything */
+  for (var i in req.body) {
+    if (required.indexOf(req.body[i])) {
+      if (!req.body[i]) {
+        errors.push('Missing field: '+i+'<br/>');
+      }
+    }
+  }
+
+  /* Send errors if they exist */
+  if (errors.length > 0) {
+
+    res.send(errors.join(' '));
+    return;
+
+  } else {
+
+    if (req.body.spkac) {
+      var response = [];
+
+      /*
+       * Convert the SPKAC to a buffer and
+       * strip the browsers need for line endings
+       */
+      var spkac = new Buffer(stripLineEndings(req.body.spkac), 'utf-8');
+
+      /* Test validity of SPKAC */
+      response.push('SPKAC valid: <em>'+spki.verifySpkac(spkac)+'</em><br/>');
+
+      /* Export challenge associated with SPKAC */
+      response.push('SPKAC Challenge: <em>'+spki.exportChallenge(spkac).toString('utf-8')+'</em><br/>');
+
+      /* Export public key associated with SPKAC */
+      response.push('SPKAC Public Key: <pre>'+spki.exportPublicKey(spkac).toString('utf-8')+'</pre><br/>');
+
+      res.send(response.join(' '));
+    } else {
+      res.send('A SPKAC was not found in POST request');
+    }
+  }
 };
 
 function stripLineEndings(obj) {
